d30e863bd47d91e84bffdf6197966ff892236a22
fachreza <rezatangse@gmail.com>
2020-07-19 15:16:11 +0700

    add dashboar faza in welcome pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    faza.web.id
+                    Dashboard Faza
                 </div>
 
                 <div class="links">
